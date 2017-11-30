af8c430d218be1a285a8ecbefd487574a70b8c2a
Nick Garcia <89nickgarcia@gmail.com>
2017-11-30 04:14:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(array){
   var array = [];
   for (let i == 0; i < 25; i++)
-} if (i == 1){
+{ if (i == 1){
   console.log("I am 1 strange loop")
 } else if (i == 2){
   console.log(`I am ${i} strange loop`)
+}
 }
